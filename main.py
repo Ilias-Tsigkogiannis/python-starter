d3bcd89d0ec267ff4d0bc1f222e6236bfda33263
Praful Rana <prafulrana@gmail.com>
2016-10-21 16:37:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ def index():
   git['shortSHA'] = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).rstrip()
   git['lastCommitTime'] = subprocess.check_output(["git", "log", "--format=\"%ai\"", "-n1", "HEAD"]).rstrip()
   git['lastCommitMessage'] = subprocess.check_output(["git", "log","--format=\"%B\"", "-n1", "HEAD"]).rstrip()
-  git['lastCommitAuthor'] = subprocess.check_output(["git", "log", "--format=\"%aN\"", "-n1", "HEAD"]).rstrip().strip("\"")
   git['remoteOriginUrl'] = subprocess.check_output(["git", "config", "--get-all", "remote.origin.url"]).rstrip()
   return render_template('index.html', git=git )
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0', port='80')
